8baaa576e4d9e1d426961c7fa8eb58d6fb982381
Engineev <ryw_pub@126.com>
2018-02-19 15:09:54 +0800

    archive

--- README.md
+++
@@ -1,4 +0,0 @@
-# scientific-computing-notes
-These are the lecture notes of SJTU course Scientific Computing.
-
-Thank Evensgn for reviewing.
--- homework/Ex08a/src/direct_iteration.m
+++
@@ -1,9 +0,0 @@
-function [res, cnt] = direct_iteration(phi, x0, eps)
-    res = phi(x0);
-    cnt = 1;
-    while abs(res - x0) >= eps
-        x0 = res;
-        res = phi(x0);
-        cnt = cnt + 1;
-    end
-end
